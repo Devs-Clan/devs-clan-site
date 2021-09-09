65f5f019820731b866f29ed406e1a19cfe067c12
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-09-09 23:58:39 +0300

    follow members

--- js/getMembers.js
+++ js/getMembers.js
@@ -1,27 +1,13 @@
-// import tokens from './tokens'
+// import  '../css/main.css'
 
-const GitHub = require('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');
- 
-var gh = new GitHub({
-  token: '<token>',
-});
 
-var getOrganizationMembers = (orgnization, page = 1, members = []) => {
-  return gh.getOrganization(orgnization).listMembers({ page: page }).then(res => {
-    members = members.concat(res.data);
+fetch("https://api.github.com/orgs/Devs-Clan/public_members")
+.then((result)=> result.json())
+.then((data) => {
 
-    if (res.headers.link.split(",").filter(l => l.match(/rel="next"/)).length > 0) {
-      return getOrganizationMembers(orgnization, page+1, members);
+    for (let i = 0; i < 100; i++) {
+        
+    document.getElementById("members").innerHTML += `<a target="_blank" href="${data[0].html_url}" ><img style="width: 50px; height: 50px; margin: 10px;" src="${data[0].avatar_url}"/></a>`
     }
 
-    return Promise.resolve(members);
-  });
-};
-
-getOrganizationMembers("<org_name>").then(members => {
-  const data = members.map((m, i) => {
-    return `${m.id}\t${m.login}\t=IMAGE("${m.avatar_url}")\t${m.type}`
-  }).join("\n");
-//   console.log(data);
-  document.getElementById("result").innerHTML = `<img src="${data.avatar_url}"/>`
-});
+})
