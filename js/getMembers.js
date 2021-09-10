3764baf8cf936b073cf562290e87f6f7de2a0103
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-09-10 03:13:34 +0300

    last commit for today

--- js/getMembers.js
+++ js/getMembers.js
@@ -1,9 +1,30 @@
 
+  // Get HTML head element
+  var head = document.getElementsByTagName('HEAD')[0]; 
+  
+  // Create new link Element
+  var link = document.createElement('link');
+
+  // set the attributes for link element 
+  link.rel = 'stylesheet'; 
+
+  link.type = 'text/css';
+
+  link.href = '../css/main.css'; 
+
+  // Append link element to HTML head
+  head.appendChild(link); 
+
 fetch("https://api.github.com/orgs/Devs-Clan/public_members")
 .then((result)=> result.json())
 .then((data) => {
 
     for (let i = 0; i < data.length; i++) {   
-    document.getElementById("members").innerHTML += `<a target="_blank" href="${data[i].html_url}" ><img style="width: 50px; height: 50px; margin: 10px;" src="${data[i].avatar_url}"/></a>`
+    document.getElementById("members").innerHTML +=
+    `
+        <a alt="member icon" target="_blank" href="${data[i].html_url}" title="${data[i].login}">
+             <img class="member-icon" src="${data[i].avatar_url}"/>
+        </a>
+    `
     }
 })
