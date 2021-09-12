0439ba713ebd3477e47bb224c89c2c71fc12bdd1
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-09-12 03:04:21 +0300

    finish suitwindow

--- js/suitWindow.js
+++ js/suitWindow.js
@@ -1,10 +1,17 @@
-const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+checkSize();
+window.addEventListener('resize', function(event) {
+    checkSize();
+}, true);
 
-if(width <= 810){
-   alert("yay")
-
-    var row = document.getElementById("row");
-    row.style.display = "grid";
-    
+function checkSize(){
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if(width <= 810){
+        document.getElementById("row").style.display = "grid";
+        document.getElementById("leftcolumn").setAttribute("style","width:100%");
+        document.getElementById("rightcolumn").setAttribute("style","width:100%");
+    }else{
+        document.getElementById("row").style.display = "table";
+        document.getElementById("leftcolumn").setAttribute("style","width:75%");
+        document.getElementById("rightcolumn").setAttribute("style","width:25%");
+    }
 }
-
