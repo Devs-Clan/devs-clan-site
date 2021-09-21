c6e2bd9e22ff7cf7c8c1096c65e8988841dfe752
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-09-21 22:27:51 +0300

    fix bug

--- js/getProjects.js
+++ js/getProjects.js
@@ -1,36 +1,42 @@
+
+
 fetch("https://api.github.com/orgs/Devs-Clan/repos")
 .then((result)=> result.json())
 .then((data) => {
 
-    for (let i = 0; i < data.length; i++) {
+        for(let i = 0; i < data.length; i++){
 
-        // var temp = "test";
+            if(data[i].language){
+        
+                var lan = "Main language " + data[i].language;
+                
+                var description = "No description";
+                if(data[i].description){
+                    description = data[i].description;
+                }
+        
+                document.getElementById("projects").innerHTML += 
+                `
+                    <div class="card" style="width:100%;">
+                        <h2>${data[i].name}</h2>
+                        <h5>Created at : ${data[i].created_at}</h5>
+                        <h6>${lan}</h6>
+                        <img src="./images/devs_clan_logo.jpg">
+                        <p>${description}</p>   
+                        <a href="${data[i].html_url}" target="_blank" style="width:100%;" class="btn"><span>SOURCE CODE</span></a>
+                    </div>
+                `   
+            }
+        }
+    })
+
+   // var temp = "test";
         // fetch("https://api.github.com/repos/Devs-Clan/Welcome/contributors")
         // .then((result2)=> result2.json()).then((data2)=>{
         //     console.log(data2[0].login)
         //     temp = data2[0].login;
         // }).catch(function(){console.log("ouch")})
         
-        var lan;
-        if(data[i].language){
-            lan = "Main language " + data[i].language;
-        }else{
-            lan = "No language";
-        }
-
-        document.getElementById("projects").innerHTML += 
-        `
-            <div class="card" style="width:100%;">
-                <h2>${data[i].name}</h2>
-                <h5>Created at : ${data[i].created_at}</h5>
-                <h6>${lan}</h6>
-                <img src="./images/devs_clan_logo.jpg">
-                <p>${data[i].description}</p>   
-                <a href="${data[i].html_url}" target="_blank" style="width:100%;" class="btn"><span>SOURCE CODE</span></a>
-            </div>
-        `   
-    }
-})
 
 
 
