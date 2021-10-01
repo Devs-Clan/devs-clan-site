285a4a594f1037af2fe926c80562ef76dec24094
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-01 12:10:53 +0300

    more firebase things

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,17 +1,37 @@
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
+// admin.initializeApp({
+//   credential: admin.credential.applicationDefault()
+//   // credential: admin.credential.cert(require('./devs-clan-firebase-adminsdk-qro16-0cada4fc0c.json')) 
+// });
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault()
-});
+// const db = admin.firestore();
 
-const db = admin.firestore();
+// const docRef = db.collection('users').doc('alovelace');
 
-const docRef = db.collection('users').doc('alovelace');
+// await docRef.set({
+//   first: 'Ada',
+//   last: 'Lovelace',
+//   born: 1815
+// });
 
-await docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
-});
+// console.log(docRef.collection)
+// import firebase from '../../firebase'
+// import * as firebase from "firebase/app";
+// const firebase = require('firebase')
 
-console.log(docRef)
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBtUcu3gpNGV0ReN7fklRDOY7QwOtTGEAg",
+//   authDomain: "devs-clan.firebaseapp.com",
+//   databaseURL: "https://devs-clan-default-rtdb.firebaseio.com",
+//   projectId: "devs-clan",
+//   storageBucket: "devs-clan.appspot.com",
+//   messagingSenderId: "606508655428",
+//   appId: "1:606508655428:web:e49aaaa6b2875964deed11",
+//   measurementId: "G-VB7X60HH19"
+// };
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+
+// var firebaseDB = firebase.database()
+
+// firebaseDB.ref('text').set('more-text')
