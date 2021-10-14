455db11eddfd96a794c8e2b5de9a79f16ba3b9a4
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-14 15:58:23 +0300

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,24 @@
 if (window.location.pathname.split("/").pop() == "homepage.html") {
-  loadScript("../js/getLinkCss.js");
-  loadScript("../js/getMembers.js");
-  loadScript("../js/getProjects.js");
-  loadScript("../js/suitWindow.js");
+  loadScript("../js/getLinkCss.js")
+  loadScript("../js/getMembers.js")
+  loadScript("../js/getProjects.js")
+  loadScript("../js/menu.js")
 }
 
 if((window.location.pathname).split("/").pop() == 'sign-in.html'){
-  loadScript("../js/sign-in.js");
+  loadScript("../js/sign-in.js")
+}
+
+if((window.location.pathname).split("/").pop() == 'requests.html'){
+  loadScript("../js/menu.js")
 }
 
 function loadScript(url) {
   var script = document.createElement("script");
   script.src = url;
   document.head.appendChild(script);
+}
+
+function goBack(){
+  window.history.go(-1)
 }
